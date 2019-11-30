bffe44363c288e321c6878ccc333f826fd9ad0a9
gitingrid <49993985+gitingrid@users.noreply.github.com>
2019-11-30 12:32:15 +0200

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,14 @@
-$('#btn').click(function(){
-    $('.fas').removeClass('fa-plus');
-    $('.fas').addClass('fa-minus');
-    $('.card').removeClass('card--blue');
-    $('.card').toggleClass('card--green');
-    $('.card-content').toggleClass('view');
-    $('.card-description').toggleClass('helper');
+
+
+$(document).ready(function(){
+  
+        $('.plus-minus-toggle').on('click', function() {
+            $(this).toggleClass('collapsed');
+            $('.card').removeClass('card--blue');
+            $('.card').toggleClass('card--green');
+            $('.card-content').toggleClass('view');
+            $('.card-description').toggleClass('helper');
+          });
+        
+
 });
